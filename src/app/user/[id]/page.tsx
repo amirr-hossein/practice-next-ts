77866483c34993059bb7029ba49089e7d1de3ff9
amirrhossein.ir <amirrhossein.ir@gmail.com>
2024-05-19 01:01:37 +0330

    update=>enum type and tuple type

--- src/app/user/[id]/page.tsx
+++ src/app/user/[id]/page.tsx
@@ -1,4 +1,28 @@
+enum Color {
+  Blue,
+  Red
+}
+const product: {
+  title: string;
+  price: number;
+  discount: [string, string];
+  color:any
+} = {
+  title: "clock",
+  price: 90000,
+  discount: ["atm", "smr"],
+  color:Color.Blue
+};
 function user() {
-    return <h1>user2</h1>;
-  }
-  export default user;
+  return <>
+      <div>
+        <h1>{product.title}</h1>
+        <span>{product.price}</span>
+        {product.discount?.map((item,index)=>(
+          <h4 key={index}>{item}</h4>
+        ))}
+        {product.color===Color.Blue?<span>i have this color</span>:<span>i dont have this color</span>}
+      </div>
+  </>;
+}
+export default user;
